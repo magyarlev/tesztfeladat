84c93cec3a62a09d1ac55d94f4ee4ef5546d70d8
MagyarL <magyar.levi@gmail.com>
2025-02-03 19:14:08 +0100

    add skeletons for transactions

--- tesztfeladat/src/app/transaction-display/transaction-display.component.ts
+++ tesztfeladat/src/app/transaction-display/transaction-display.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TransactionItemComponent } from './transaction-item/transaction-item.component';
 import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { PaymentsService } from '../payments.service';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-transaction-display',
-  imports: [TransactionItemComponent, NgbAccordionModule],
+  imports: [TransactionItemComponent, NgbAccordionModule, NgbNavModule],
   templateUrl: './transaction-display.component.html',
   styleUrl: './transaction-display.component.scss',
 })
-export class TransactionDisplayComponent {}
+export class TransactionDisplayComponent {
+  paymentsService = inject(PaymentsService);
+  payments = this.paymentsService.getPayments();
+}
