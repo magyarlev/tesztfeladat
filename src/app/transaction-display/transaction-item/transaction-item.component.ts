37eb56f02d16b00259f474d19d16c49f9bbfbb17
MagyarL <magyar.levi@gmail.com>
2025-02-04 12:06:15 +0100

    add transaction item html and styling

--- src/app/transaction-display/transaction-item/transaction-item.component.ts
+++ src/app/transaction-display/transaction-item/transaction-item.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { PaymentsService } from '../../payments.service';
+import { NgClass } from '@angular/common';
+import { Status } from '../../../types';
 
 @Component({
   selector: 'app-transaction-item',
-  imports: [],
+  imports: [NgbAccordionModule, NgClass],
   templateUrl: './transaction-item.component.html',
-  styleUrl: './transaction-item.component.scss'
+  styleUrl: './transaction-item.component.scss',
 })
 export class TransactionItemComponent {
-
+  paymentsService = inject(PaymentsService);
+  payments = this.paymentsService.getPayments();
 }
