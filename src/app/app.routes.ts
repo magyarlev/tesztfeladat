27ebd7696a076963750dd2a03dfad24311176aba
MagyarL <magyar.levi@gmail.com>
2025-02-04 12:06:24 +0100

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { TransactionItemComponent } from './transaction-display/transaction-item/transaction-item.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'items1',
+    component: TransactionItemComponent,
+  },
+  {
+    path: 'items2',
+    component: TransactionItemComponent,
+  },
+  {
+    path: 'items3',
+    component: TransactionItemComponent,
+  },
+];
